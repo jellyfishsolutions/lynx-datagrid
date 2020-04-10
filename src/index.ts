fc47147ca75a769c23d5debb58e1cbfb125dc2cf
Luca Roverelli <luca.roverelli@gmail.com>
2020-04-10 17:52:11 +0200

    fixed module settings

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import SimpleModule from "lynx-framework/simple.module";
 
 export default class DatagridModule extends SimpleModule {
-
-    get controllers(): string {
-        return __dirname + "/controllers";
-    }
-
     get views(): string {
         return __dirname + "/views";
-    }
-
-    get public(): string {
-        return __dirname + "/public";
     }
 }
